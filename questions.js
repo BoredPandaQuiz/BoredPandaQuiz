a4aedbd030684017ccccf30b0c054e795efc104c
BoredPandaQuiz <tomas.kn@boredpanda.com>
2026-03-20 15:26:39 +0200

    Update questions.js

--- questions.js
+++ questions.js
@@ -26,5 +26,97 @@ const QUESTIONS = [
     question: "How many languages does Bored Panda use to distribute its content?",
     answers: ["21", "17", "12"],
     correctAnswer: "21"
+  },
+  {
+    id: "5",
+    question: "How many people work at Bored Panda?",
+    answers: ["around 500", "around 1000", "around 350"],
+    correctAnswer: "around 350"
+  },
+  {
+    id: "6",
+    question: "What was the #1 Facebook page in 2017?",
+    answers: ["Bored Panda", "Ladbible", "Crafty Panda"],
+    correctAnswer: "Bored Panda"
+  },
+  {
+    id: "7",
+    question: "What was the #1 Facebook page in 2020?",
+    answers: ["Crafty Panda", "5-minute Crafts", "Gotcha!"],
+    correctAnswer: "Crafty Panda"
+  },
+  {
+    id: "8",
+    question: "Bored Panda was founded in 2009. In what circumstances was it started?",
+    answers: [
+      "While working in a similar company",
+      "While studying at the university",
+      "While spending time jobless"
+    ],
+    correctAnswer: "While studying at the university"
+  },
+  {
+    id: "9",
+    question: "What year was Bored Panda founded?",
+    answers: ["2021", "2009", "2017"],
+    correctAnswer: "2009"
+  },
+  {
+    id: "10",
+    question: "When did Bored Panda start producing videos?",
+    answers: ["2011", "2017", "2021"],
+    correctAnswer: "2017"
+  },
+  {
+    id: "11",
+    question: "What is the name of our animated series available on YouTube and Go3?",
+    answers: ["Fire spike", "Peppa Pig", "PAW Patrol"],
+    correctAnswer: "Fire spike"
+  },
+  {
+    id: "12",
+    question: "What is the Lithuanian title of the Fire Spike animated series?",
+    answers: ["Mažasis Ugnies Drakonas", "Ugniukas Drakoniukas", "Drakoniukas Spygliukas"],
+    correctAnswer: "Drakoniukas Spygliukas"
+  },
+  {
+    id: "13",
+    question: "Bored Panda has many contractors working worldwide. Do you know approximately how many countries they come from?",
+    answers: ["20", "42", "36"],
+    correctAnswer: "36"
+  },
+  {
+    id: "14",
+    question: "How many offices does Bored Panda have in Vilnius?",
+    answers: ["2", "3", "4"],
+    correctAnswer: "2"
+  },
+  {
+    id: "15",
+    question: "In which cities does Bored Panda have offices?",
+    answers: ["Vilnius and Kaunas", "Vilnius and Klaipėda", "Only in Vilnius"],
+    correctAnswer: "Only in Vilnius"
+  },
+  {
+    id: "16",
+    question: "What brand is featured on the Mr. Brick YouTube channel?",
+    answers: ["Minecraft", "LEGO", "Roblox"],
+    correctAnswer: "LEGO"
+  },
+  {
+    id: "17",
+    question: "Which Bored Panda YouTube projects use real actors in their videos?",
+    answers: ["Gotcha!", "Mr.Bricks", "Fire Spike"],
+    correctAnswer: "Gotcha!"
+  },
+  {
+    id: "18",
+    question: "What type of content is published on boredpanda.com?",
+    answers: [
+      "In-depth political analysis and investigative world news reporting",
+      "Technical articles focused on business strategy, finance, and economics",
+      "Pop culture, entertainment news, and lifestyle stories"
+    ],
+    correctAnswer: "Pop culture, entertainment news, and lifestyle stories"
   }
 ];
