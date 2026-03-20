f4c1e1d8d4e8c25fdf4815946874f0bec9adba98
BoredPandaQuiz <tomas.kn@boredpanda.com>
2026-03-20 15:24:01 +0200

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,30 @@
+const QUESTIONS = [
+  {
+    id: "1",
+    question: "How many YouTube Diamond Buttons has Bored Panda earned?",
+    answers: ["4", "10", "1"],
+    correctAnswer: "1"
+  },
+  {
+    id: "2",
+    question: "How many video views do we generate per year?",
+    answers: ["51 billion views", "100 billion views", "62 billion views"],
+    correctAnswer: "62 billion views"
+  },
+  {
+    id: "3",
+    question: "What’s Bored Panda’s motto?",
+    answers: [
+      "Multiply your creativity with us!",
+      "It's all about entertaining people!",
+      "Let's fight boredom together!"
+    ],
+    correctAnswer: "Let's fight boredom together!"
+  },
+  {
+    id: "4",
+    question: "How many languages does Bored Panda use to distribute its content?",
+    answers: ["21", "17", "12"],
+    correctAnswer: "21"
+  }
+];
